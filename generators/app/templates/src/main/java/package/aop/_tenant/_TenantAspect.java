fdc011d40de7e4dc7df3ccf7de71222cdbd71e8c
Mairead McCabe <mairead.mccabe@sonalake.com>
2017-09-08 12:35:49 +0100

    JMT-4: Update aspect imports, filter logic

--- generators/app/templates/src/main/java/package/aop/_tenant/_TenantAspect.java
+++ generators/app/templates/src/main/java/package/aop/_tenant/_TenantAspect.java
@@ -1,13 +1,17 @@
 package <%=packageName%>.aop.company;
 
-import <%=packageName%>.service.UserService;
-import org.aspectj.lang.JoinPoint;
+import <%=packageName%>.security.AuthoritiesConstants;
+import <%=packageName%>.security.SecurityUtils;
+import <%=packageName%>.repository.UserRepository;
+import <%=packageName%>.domain.User;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.hibernate.Filter;
 
 @Aspect
 @Component
@@ -15,16 +19,26 @@ public class <%= tenantNameUpperFirst %>Aspect {
  
     @PersistenceContext
     private EntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private final String fieldName =  "<%= tenantNameSpinalCased %>Id";
+
     /**
-     * Run method if either a class or a method is annotated with `@UserFilter`
+     * Run method if User service is hit.
      * Filter users based on which <%= tenantNameUpperFirst %> the user is associated with.
      * Skip filter if user has ROLE_ADMIN, they can view all <%= tenantNameUpperFirst %>
      *
-     * @param pjp
-     * @return
-     * @throws Throwable
      */
     @Before("execution(* com.sonalake.multitenancy.service.UserService.*(..))")
-    public void aroundExecution(JoinPoint pjp, UserService userService) throws Throwable {
+    public void beforeExecution() throws Throwable {
+        // admin users results should NOT be filtered
+        if (!SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN)) {
+            String login = SecurityUtils.getCurrentUserLogin();
+            User user = userRepository.findOneByLogin(login).get();
+            Filter filter = entityManager.unwrap(Session.class).enableFilter("USER_FILTER");
+            filter.setParameter(fieldName, user.get<%= tenantNameUpperFirst %>().getId());
+        }
     }
 }
