af3162a08d368bfef21bb5a5c6fe6ae859bfc2eb
Mairead McCabe <mairead.mccabe@danutech.com>
2017-09-29 10:47:31 +0100

    JMT-43: subgenerator init test

---
+++ test/test-entity.spec.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fse = require('fs-extra');
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const chalk = require('chalk');
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+
+describe('Multitenancy sub generator', () => {
+    describe('Given name argument', () => {
+        describe('jhipster-multitenancy module installed', () => {
+            it.only('throws an error as entity doesn\'t exist', () => {
+                helpers
+                    .run(path.join(__dirname, '../generators/entity'))
+                    .inTmpDir((dir) => {
+                        fse.copySync(path.join(__dirname, '../test/templates/module-installed'), dir);
+                    })
+                    .withOptions({
+                        testmode: true
+                    })
+                    .withArguments(['foo'])
+                    .on('error', (e) => {
+                        const errorMsg = `${e} `;
+                        assert.equal(true, errorMsg.indexOf(chalk.yellow(`Entity ${chalk.bold('foo')} doesn't exist. Please generate using yo jhipster:entity foo`)) >= 0);
+                    });
+            });
+        });
+    });
+});
