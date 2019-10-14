3d8f7a08eea9d73c72362ddb40d7fa047cd240f9
Charlotte Fawsitt <charlotte.fawsitt@sonalake.com>
2019-10-14 15:55:51 +0100

    Text fixes

--- test/server.spec.js
+++ test/server.spec.js
@@ -4,7 +4,7 @@ const helpers = require('yeoman-test');
 const fse = require('fs-extra');
 
 describe('Subgenerator server of multitenancy JHipster blueprint', () => {
-    describe('Sample test', () => {
+    describe('Validation check for "SQL" database type', () => {
         before(done => {
             helpers
                 .run('generator-jhipster/generators/server')
@@ -26,32 +26,9 @@ describe('Subgenerator server of multitenancy JHipster blueprint', () => {
                 ])
                 .on('end', done);
         });
-    });
-});
 
-describe('Validation check for "SQL" database type', () => {
-    before(done => {
-        helpers
-            .run('generator-jhipster/generators/server')
-            .inTmpDir(dir => {
-                fse.copySync(path.join(__dirname, './templates/default'), dir);
-            })
-            .withOptions({
-                'from-cli': true,
-                skipInstall: true,
-                blueprint: 'multitenancy',
-                skipChecks: true
-            })
-            .withGenerators([
-                [
-                    require('../generators/server/index.js'), // eslint-disable-line global-require
-                    'jhipster-multitenancy:server',
-                    path.join(__dirname, '../generators/server/index.js')
-                ]
-            ])
-            .on('end', done);
-    });
-    it('contains databaseType with sql value', () => {
-        assert.fileContent('.yo-rc.json', /"databaseType": "sql"/);
+        it('contains databaseType with sql value', () => {
+            assert.fileContent('.yo-rc.json', /"databaseType": "sql"/);
+        });
     });
 });
