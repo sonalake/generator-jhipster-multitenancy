eb652748f330f0cafaa74170fa9e6c375e0ac723
Charlotte Fawsitt <charlotte.fawsitt@sonalake.com>
2019-10-29 12:35:18 +0000

    Removed company from entity menu and moved into admin menu. This also removes the extra entity menu item.

--- generators/entity-client/partials/angular/navbar.component.html.js
+++ generators/entity-client/partials/angular/navbar.component.html.js
@@ -4,8 +4,25 @@ const tmpls = [
     {
         type: 'replaceContent',
         regex: true,
-        target: context => `routerLink="${context.tenantNameLowerFirst}"`,
-        tmpl: context => `routerLink="${context.tenantNameLowerFirst}" *jhiHasAnyAuthority="'ROLE_ADMIN'"`
+        target: context => `<li>\n(\\s*)<a(.*)routerLink="${context.tenantNameLowerFirst}"([^]+)Company</span>(\\s*)</a>(\\s*)</li>`,
+        tmpl: context => ''
+    },
+    {
+        type: 'replaceContent',
+        regex: true,
+        target: context => `<li>\n(\\s*)<a(.*)routerLink="admin/user-management"([^]+)User(\\s)management</span>(\\s*)</a>(\\s*)</li>`, // eslint-disable-line
+        tmpl: context => `<li>
+                        <a class="dropdown-item" routerLink="${context.tenantNameLowerFirst}" routerLinkActive="active" [routerLinkActiveOptions]="{ exact: true }" (click)="collapseNavbar()">
+                            <fa-icon icon="asterisk" fixedWidth="true"></fa-icon>
+                            <span jhiTranslate="${context.baseName}App.${context.tenantNameLowerFirst}.home.title">${context.tenantNamePluralLowerFirst}</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" routerLink="admin/user-management" routerLinkActive="active" (click)="collapseNavbar()">
+                            <fa-icon icon="user" fixedWidth="true"></fa-icon>
+                            <span jhiTranslate="global.menu.admin.userManagement">User management</span>
+                        </a>
+                    </li>`
     }
 ];
 
