e0aeaf5a7bd501fb0c11585e97daf0c6e6610181
Charlotte Fawsitt <charlotte.fawsitt@sonalake.com>
2019-11-06 14:07:18 +0000

    Circular reference on user request fix.

--- generators/entity-server/partials/server/_TenantResource.js
+++ generators/entity-server/partials/server/_TenantResource.js
@@ -3,14 +3,6 @@ const file = context => `${context.SERVER_MAIN_SRC_DIR}${context.packageFolder}/
 const tmpls = [
     {
         type: 'rewriteFile',
-        target: context => `${context.tenantNameLowerFirst}Service.delete(id);`,
-        tmpl: context => `${context.tenantNameUpperFirst} ${context.tenantNameLowerFirst} = ${context.tenantNameLowerFirst}Service.findOne(id).orElse(null);
-        if(${context.tenantNameLowerFirst} == null || !${context.tenantNameLowerFirst}.getUsers().isEmpty()){
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(applicationName, true, ENTITY_NAME, "deletefail", "Delete Failed. Please remove users first")).build();
-        }`
-    },
-    {
-        type: 'rewriteFile',
         target: context => `public class ${context.tenantNameUpperFirst}Resource {`,
         tmpl: context => '@PreAuthorize("hasAnyRole(\\"" + AuthoritiesConstants.ADMIN + "\\")")'
     },
